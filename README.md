a5fb49cc91465e5e57469ab513b0e68421e29c6d
Claude Ayitey <clauditey@gmail.com>
2014-08-31 06:13:37 +0000

    initial commit, more like final commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+boughspot.com
+=============
+
+Our website
---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from datetime import datetime
+from flask import (
+   Flask,
+   url_for,
+   render_template,
+   redirect,
+   request,
+   flash
+)
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
